686bfed9aa10c12d92a497fce10a6b4833bb5b0e
sissis12 <115008914+sissis12@users.noreply.github.com>
2022-12-27 13:05:06 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
 })
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(1)
+})
+basic.showIcon(IconNames.Heart)
